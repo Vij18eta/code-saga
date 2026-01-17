b7ad579cad0eb5a4f8e332e60a16d7ee51b61555
VIJETA VIJAYKUMAR NAIK <vijetaviju18@gmail.com>
2026-01-17 23:24:43 +0530

    Implement tree traversal functions
    Added functions for inorder, preorder, and postorder tree traversal.

--- trees/traversal.py
+++ trees/traversal.py
@@ -11,3 +11,26 @@ root.left.right=TreeNode(4)
 root.left.left=TreeNode(5)
 
 #inorder traversal
+def inorder(root):
+  if root is None:
+    return
+  inorder(root.left)
+  print(root.value,end="")
+  inorder(root.right)
+
+#preorder traversal
+def preorder(root):
+  if root is None:
+    return
+  print(root.value,end="")
+  preorder(root.left)
+  preorder(root.right)
+
+#postorder traversal
+def postorder(root):
+  if root is none
+   return
+ postorder(root.left)
+postorder(root.right)
+print(root.value,end="")
+
